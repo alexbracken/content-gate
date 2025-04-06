b105f5cafe3b151bc42574a716b400fc3435858f
alexbracken <alex@alexbracken.co>
2025-04-06 17:01:25 -0400

    Add header settings

--- settings.php
+++ settings.php
@@ -8,6 +8,7 @@ add_action( 'admin_menu', 'cg_settings_page' );
 function cg_register_settings() {
     register_setting( 'cg_settings_group', 'cg_recaptcha_site_key' );
     register_setting( 'cg_settings_group', 'cg_recaptcha_secret_key' );
+    register_setting( 'cg_settings_group', 'cg_form_message' );
 }
 add_action( 'admin_init', 'cg_register_settings' );
 
@@ -20,12 +21,23 @@ function cg_settings_page_html() {
             <?php do_settings_sections( 'cg_settings_group' ); ?>
             <table class="form-table">
                 <tr>
+                    <th>Form Message:</th>
+                    <td>
+                        <textarea name="cg_form_message" rows="3" cols="50"><?php echo esc_textarea( get_option('cg_form_message', 'This content is protected.') ); ?></textarea>
+                        <p class="description">Message displayed to users above the form</p>
+                    </td>
+                </tr>
+                <tr>
                     <th>reCAPTCHA Site Key:</th>
-                    <td><input type="text" name="cg_recaptcha_site_key" value="<?php echo esc_attr( get_option('cg_recaptcha_site_key') ); ?>" /></td>
+                    <td>
+                        <input type="text" name="cg_recaptcha_site_key" value="<?php echo esc_attr( get_option('cg_recaptcha_site_key') ); ?>" />
+                    </td>
                 </tr>
                 <tr>
                     <th>reCAPTCHA Secret Key:</th>
-                    <td><input type="text" name="cg_recaptcha_secret_key" value="<?php echo esc_attr( get_option('cg_recaptcha_secret_key') ); ?>" /></td>
+                    <td>
+                        <input type="text" name="cg_recaptcha_secret_key" value="<?php echo esc_attr( get_option('cg_recaptcha_secret_key') ); ?>" />
+                    </td>
                 </tr>
             </table>
             <?php submit_button(); ?>
