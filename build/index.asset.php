db2702ad9b3ad339564f554620d31496804f1b2b
alexbracken <alex@alexbracken.co>
2025-05-04 11:47:14 -0400

    Update block styleing

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '0b1dd69cc25a4eeaf2ca');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => 'b63062f5fa4a219c3e18');
---
+++ build/style.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => '95617d80fbdd9fda3c51');
